c1e621c56ac6000253c144c96147bc81a5064891
Mateus Pereira <mateus.p.dias@hotmail.com>
2020-08-07 18:38:46 -0300

    Adicionando produto no menu de navegação

--- QuickBuy.web/ClientApp/src/app/produto/produto.component.ts
+++ QuickBuy.web/ClientApp/src/app/produto/produto.component.ts
@@ -1,7 +1,7 @@
 import { Component } from "@angular/core";
 
 @Component({
-  selector: "produto", // nome da tag que irá representar o componente
+  selector: "app-produto", // nome da tag que irá representar o componente
   template: "<html><body>{{ obterNome() }}</body></html>"
 })
 
