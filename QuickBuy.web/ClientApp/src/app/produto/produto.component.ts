876eba1bc788030708672a5d2288c3cdfe7999a0
Mateus Pereira <mateus.p.dias@hotmail.com>
2020-08-07 17:49:48 -0300

    Adicionando referência do produto ao app.module

--- QuickBuy.web/ClientApp/src/app/produto/produto.component.ts
+++ QuickBuy.web/ClientApp/src/app/produto/produto.component.ts
@@ -1,5 +1,16 @@
-export class ProdutoComponent{
-  public id: number;
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "produto", // nome da tag que irá representar o componente
+  template: "<html><body>{{ obterNome() }}</body></html>"
+})
+
+export class ProdutoComponent {
+
   public nome: string;
-  public preco: number;
+  public liberadoParaVenda: boolean;
+
+  public obterNome(): string {
+    return "Samsumg";
+  }
 }
