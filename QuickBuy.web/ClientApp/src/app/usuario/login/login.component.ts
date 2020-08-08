5822491f43098d174c4a7095b0c24ac8905f5b46
Mateus Pereira <mateus.p.dias@hotmail.com>
2020-08-08 17:14:13 -0300

    Binding simples com interpolação de string

--- QuickBuy.web/ClientApp/src/app/usuario/login/login.component.ts
+++ QuickBuy.web/ClientApp/src/app/usuario/login/login.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: "login",
-  templateUrl: "./login.component.html",
-  styleUrls: ["./login.component.css"]
+  selector: 'login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
 })
 
 export class LoginComponent {
-
+  public email = "mateus.teste@gmail.com";
+  public senha = "abc213";
 }
