a76790824faaf7c9b0f5aa297ffdaae3d1ce570d
Mateus Pereira <mateus.p.dias@hotmail.com>
2020-08-14 10:25:48 -0300

    Diretiva estrutural *ngFor

--- QuickBuy.web/ClientApp/src/app/usuario/login/login.component.ts
+++ QuickBuy.web/ClientApp/src/app/usuario/login/login.component.ts
@@ -10,6 +10,7 @@ import { Usuario } from '../../modelo/usuario';
 export class LoginComponent {
   public usuario;
   public usuarioAutenticado: boolean;
+  public usuarios = ["usuario1", "usuario2", "usuario3", "usuario4", "usuario5"];
 
   constructor() {
     this.usuario = new Usuario();
